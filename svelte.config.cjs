00b236f740c26d1b34c61bc91abffb54ed21837c
Tim Smith <gonad2019@gmail.com>
2021-08-18 11:23:26 -0700

    fdfd

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,5 @@
-import sveltePreprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify';
+const sveltePreprocess = require('svelte-preprocess');
+const netlify = require('@sveltejs/adapter-netlify');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
